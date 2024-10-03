e98e4f207d4f3f24ebf1b31ea92e1a91fef52840
VDT1805 <136230459+VDT1805@users.noreply.github.com>
2024-10-03 20:04:01 +0700

    project init

---
+++ miniurl-backend/src/main/java/com/vdtoan/miniurl_backend/MiniurlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vdtoan.miniurl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniurlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniurlBackendApplication.class, args);
+	}
+
+}
---
+++ miniurl-backend/src/test/java/com/vdtoan/miniurl_backend/MiniurlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdtoan.miniurl_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniurlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

39ab80023ad393a8bef4ff1d97ab76e58565d16a
VDT1805 <136230459+VDT1805@users.noreply.github.com>
2024-10-04 20:39:59 +0700

    feat: Implement Model

---
+++ miniurl-backend/src/main/java/com/vdtoan/miniurl_backend/models/UrlModel.java
@@ -0,0 +1,23 @@
+package com.vdtoan.miniurl_backend.models;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "urls")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UrlModel {
+    @Id
+    private String id; // shortCodeURL
+    private String originalUrl;
+    @Indexed(expireAfterSeconds = 0)
+    private LocalDateTime expiredAt;
+}
